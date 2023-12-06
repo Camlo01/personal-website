2f5f464a5ef8a1fac302cc571a1190c504c7360f
Camilo <camilobeltran174@gmail.com>
2023-12-06 16:55:25 -0500

    Added about section with its styles

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,40 @@ function Header() {
   )
 }
 
+function About() {
+  const { t } = useTranslation(["main"])
+  return (
+    <section className="about">
+      <div className="about__container">
+        <div className="about__body">
+          <h2>{t('about__body-h2')}</h2>
+          <p>{t('about__body-p1')}</p>
+          <p>{t('about__body-p2')}</p>
+        </div>
+        <div className="about__photo">
+          <h2>{t('about__photo-h2')}</h2>
+          <p>{t('about__photo-p')}</p>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function Features() {
+  return (
+    <section className="features">
+      {/* Content for features section */}
+    </section>
+  )
+}
+
 function App() {
   return (
     <>
       <Header />
+      <About />
+      <Features />
     </>
   )
 }
