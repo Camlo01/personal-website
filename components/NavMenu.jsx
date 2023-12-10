65d49afc91de1f796000543665154210398c163d
Camilo <camilobeltran174@gmail.com>
2023-12-09 19:18:00 -0500

    Improve shadow and logic to toggle menu with nav buttons

--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function navbarStyles() {
     const colorToApply = "#1C1C1D"
-    const smoothShadow = "0px 2.1px 7.8px rgba(0, 0, 0, 0.015), 0px 3.8px 18.8px rgba(0, 0, 0, 0.054), 0px 5.6px 36.2px rgba(0, 0, 0, 0.151), 0px 9px 80px rgba(0, 0, 0, 0.2)";
+    const smoothShadow = " 0px 3.9px 0.3px -6px rgba(0, 0, 0, 0.183), 0px 4.9px 0.7px -6px rgba(0, 0, 0, 0.178), 0px 5.4px 1.1px -6px rgba(0, 0, 0, 0.173), 0px 5.6px 1.7px -6px rgba(0, 0, 0, 0.177), 0px 5.7px 2.4px -6px rgba(0, 0, 0, 0.191), 0px 5.7px 3.4px -6px rgba(0, 0, 0, 0.213), 0px 5.5px 4.8px -6px rgba(0, 0, 0, 0.242), 0px 5.3px 6.9px -6px rgba(0, 0, 0, 0.276), 0px 5.1px 10.7px -6px rgba(0, 0, 0, 0.316), 0px 5px 19px -6px rgba(0, 0, 0, 0.36) ";
     const navbar = document.getElementById('navbar-container');
     const position = window.scrollY
 
@@ -29,7 +29,10 @@ function NavMenu() {
     const [menuActive, setMenuActive] = useState(false);
 
     const toggleMenu = () => {
-        setMenuActive(!menuActive);
+        const isBurgerMenuActive = (window.innerWidth < 768)
+        if (isBurgerMenuActive) {
+            setMenuActive(!menuActive);
+        }
     };
 
     const changeLanguaje = () => {
