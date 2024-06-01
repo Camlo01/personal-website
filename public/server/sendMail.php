4a198e74288d100c7aeb6b0fa283f3a6bf04663f
Camilo <camilobeltran174@gmail.com>
2024-06-01 08:40:05 -0500

    Fix mail

--- public/server/sendMail.php
+++ public/server/sendMail.php
@@ -1,54 +1,39 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+use PHPMailer\PHPMailer\PHPMailer;
 
-    $config = [
-        'host' => 'smtp.hostinger.com',
-        'port' => 587,
-        'username' => 'form-no-reply@camilobeltran.com',
-        'password' => 'nektac-dubbiw-keMzy2'
-    ];
+require 'vendor/autoload.php';
 
-    // Configure the SMTP Server
-    ini_set("SMTP", $config['host']);
-    ini_set("smtp_port", $config['port']);
-    ini_set("username", $config['username']);
-    ini_set("password", $config['password']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // set the values into variables
-    $data = json_decode(file_get_contents("php://input"), true);
+    $data = json_decode(file_get_contents("php://input"));
 
-    $name = $data['name'];
-    $email = $data['email'];
-    $message = $data['message'];
+    if (json_last_error() === JSON_ERROR_NONE) {
+        $name = $data->name;
+        $email = $data->email;
+        $message = $data->message;
 
-    // Configure mail content
-    $to = "contact@camilobeltran.com";
-    $subject = "Contact Form -> New Message!";
-    $messageBody = "Nombre: $name\nCorreo electrónico: $email\nMensaje: $message";
-
-
-    // Configure headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    // Encabezados de autenticación
-    $headers .= "Return-Path: <$email>\r\n";
-    $headers .= "X-Sender: $email\r\n";
-    $headers .= "X-Mailer: PHP\r\n";
-    $headers .= "X-Priority: 1\r\n";
-
-    if (mail($to, $subject, $messageBody, $headers)) {
-        $response = array("success" => true, "message" => "Correo enviado correctamente");
-        http_response_code(200);
-        echo json_encode($response);
-    } else {
-        $response = array("success" => false, "message" => "Error al enviar el correo");
-        http_response_code(500);
-        echo json_encode($response);
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.hostinger.com';
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->Username = 'form-no-reply@camilobeltran.com';
+        $mail->Password = 'nektac-dubbiw-keMzy2';
+        $mail->setFrom('form-no-reply@camilobeltran.com', 'Message Email');
+        $mail->addAddress('contact@camilobeltran.com', 'Camilo Beltrán');
+        $mail->addReplyTo($email, $name);
+        $mail->Subject = 'New Message! -> Contact Form';
+        $mail->isHTML(false);
+        $mail->Body = "Email: " . $email . " \n !";
+        if (!$mail->send()) {
+            $response = array("success" => false, "message" => "Error al enviar el correo");
+            http_response_code(500);
+            echo json_encode($response);
+        } else {
+            $response = array("success" => true, "message" => "Correo enviado correctamente");
+            http_response_code(200);
+            echo json_encode($response);
+        }
     }
 }
-
-?>
