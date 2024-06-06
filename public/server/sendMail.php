275c81e9a65a4e01497b6bfdae93bf388c7d4c4d
Camilo <camilobeltran174@gmail.com>
2024-06-06 12:21:40 -0500

    Add allowed origins variations for website

--- public/server/sendMail.php
+++ public/server/sendMail.php
@@ -1,5 +1,19 @@
 <?php
 
+$allowed_origins = [
+    'https://camilobeltran.com',
+    'https://www.camilobeltran.com',
+    'http://camilobeltran.com',
+    'http://www.camilobeltran.com',
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 use PHPMailer\PHPMailer\PHPMailer;
 
 require 'vendor/autoload.php';
